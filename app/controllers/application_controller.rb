70faab952a5f828c7c67c80c91f23b547b35ee8b
Arvind Panwar <arvind.panwar@ArvindPanwarMac.local>
2016-11-03 11:58:57 +0530

    Update Gemfile for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 	
 	def hello 
-		render html : "Hello New App"
+		render html: "Hello New App"
 	end
 end
